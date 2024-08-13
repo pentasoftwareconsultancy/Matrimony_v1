87021f18d7a51eaa0d1e2fbdde3d0087235978c9
Vaishali S <vaishalisingare91@gmail.com>
2024-08-13 17:50:13 +0530

    inital changes

--- Client/src/Components/UserComponents/homeComponents/Hero/Hero.jsx
+++ Client/src/Components/UserComponents/homeComponents/Hero/Hero.jsx
@@ -1,11 +1,14 @@
 
+import styles from './Hero.module.css'; // Import the CSS module
 
 function Hero() {
   return (
-    <div>
-      Hero
+    <div className={styles.home}>
+
+      {/* You can add additional content here if needed */}
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
+
--- Client/src/Pages/UserPages/HomePage/HomePage.jsx
+++ Client/src/Pages/UserPages/HomePage/HomePage.jsx
@@ -1,13 +1,15 @@
-import About from "../../../Components/UserComponents/homeComponents/About/About";
+// import About from "../../../Components/UserComponents/homeComponents/About/About";
 import Footer from "../../../Components/UserComponents/homeComponents/Footer/Footer";
 import Hero from "../../../Components/UserComponents/homeComponents/Hero/Hero";
+import AboutSection from "../../../Components/UserComponents/homeComponents/lagngath/AboutSection";
+import ProfileSteps from "../../../Components/UserComponents/homeComponents/profilestep/ProfileSteps";
 
 function HomePage() {
   return (
     <div>
       <Hero/>
-      
-      <About />
+      <ProfileSteps /> 
+      <AboutSection/>
       <Footer/>
     </div>
   );
