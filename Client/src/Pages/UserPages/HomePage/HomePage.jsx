ac85d34c6c35f8c6d3920b04e420a5a6307c203f
Vaishali S <vaishalisingare91@gmail.com>
2024-08-21 22:13:21 +0530

    changes done

--- Client/src/Pages/UserPages/HomePage/HomePage.jsx
+++ Client/src/Pages/UserPages/HomePage/HomePage.jsx
@@ -1,29 +1,28 @@
-<<<<<<< HEAD
+
 // import About from "../../../Components/UserComponents/homeComponents/About/About";
 import Footer from "../../../Components/UserComponents/homeComponents/Footer/Footer";
-=======
 
 
 import About from "../../../Components/UserComponents/homeComponents/About/About";
 
->>>>>>> f0d0462c3f4f1f6676f895b0bf71053234e73743
+
 import Hero from "../../../Components/UserComponents/homeComponents/Hero/Hero";
 import AboutSection from "../../../Components/UserComponents/homeComponents/lagngath/AboutSection";
 import ProfileSteps from "../../../Components/UserComponents/homeComponents/profilestep/ProfileSteps";
+import StatsSection from "../../../Components/UserComponents/homeComponents/StatsSection/StatsSection";
 
 function HomePage() {
   return (
     <div>
       <Hero/>
-<<<<<<< HEAD
+
       <ProfileSteps /> 
       <AboutSection/>
+      <StatsSection/>
       <Footer/>
-=======
      
       <About />
 
->>>>>>> f0d0462c3f4f1f6676f895b0bf71053234e73743
     </div>
   );
 }
