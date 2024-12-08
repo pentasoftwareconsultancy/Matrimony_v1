31faf35d5c40d7fe2a36413eb0d6ee5859a2e0bb
bhagyashree-garakate <bhagyashreed1709@gmail.com>
2024-12-08 09:36:21 +0530

    changes in name and emailid, caste and footer

--- Client/src/Components/UserComponents/Footer/Footer.jsx
+++ Client/src/Components/UserComponents/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <div className={styles.footerSection}>
-          <h2>Maratha Vishv</h2>
+          <h2>Akhil Bharatiy Kunabi Samaj</h2>
           <p>Find your perfect match with us – where love meets destiny. Explore a world of trusted connections and meaningful relationships. Your journey to forever begins here, with us by your side every step of the way.</p>
         </div>
         <div className={styles.footerSection}>
